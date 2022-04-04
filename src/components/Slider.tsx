0f6973eedd2da928fb68837182cdf38d0835234f
Nabanita Sarkar <nabanita.sarkar1418@gmail.com>
2022-04-04 13:20:41 +0530

    touch slider move fix

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -22,18 +22,16 @@ export default function Slider({
         max={max}
         value={[value]}
         onValueChange={([val]) => onChange(val)}
-        className="relative flex items-center h-5 w-full"
+        className="relative flex items-center h-5 w-full select-none touch-none"
       >
         <RadixSlider.Track className="relative bg-slate-200 w-full flex-grow-1 rounded-md h-1">
           <RadixSlider.Range className="absolute bg-slate-400 rounded-md h-full" />
         </RadixSlider.Track>
-        <RadixSlider.Thumb asChild>
-          <button
-            onPointerUp={() => onMouseUp()}
-            onPointerDown={() => onMouseDown()}
-            className="block w-5 h-5 bg-white border-2 border-slate-300 shadow-sm rounded-xl focus:ring-2 focus:ring-slate-400/40 focus:outline-none"
-          />
-        </RadixSlider.Thumb>
+        <RadixSlider.Thumb
+          onPointerUp={() => onMouseUp()}
+          onPointerDown={() => onMouseDown()}
+          className="block w-5 h-5 bg-white border-2 border-slate-300 shadow-sm rounded-xl focus:ring-2 focus:ring-slate-400/40 focus:outline-none"
+        />
       </RadixSlider.Root>
     </form>
   );
