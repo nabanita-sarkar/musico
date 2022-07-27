17262b1637b7632f861a081e47caf95a8f64e416
Nabanita Sarkar <nabanita.sarkar1418@gmail.com>
2022-07-27 20:59:43 +0530

    vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/musico/",
+  base: "./",
   plugins: [react()],
   define: {
     "process.env": {},
   },
-  build: {
-    outDir: "build",
-  },
+  // build: {
+  //   outDir: "build",
+  // },
   // server: {
   //   origin: "http://localhost:3000",
   //   // host: "117.241.237.162",
